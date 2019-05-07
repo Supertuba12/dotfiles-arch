892d958d6366666e5ab769a6c27f3cab15c24123
supertuba12 <hampus.erikssonco@live.se>
2019-05-07 15:26:55 +0200

    Updated config files. Backed up installed packages.

--- .zshrc
+++ .zshrc
@@ -29,4 +29,12 @@ function workon() {
   source $HOME/virtualenvs/$1/bin/activate
 }
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk/
-export PATH=$JAVA_HOME/bin:$PATH;
+export PATH=$HOME/scripts:$JAVA_HOME/bin:$PATH;
+export PRODUCTION=false # Controls whether to use HTTPS + EMAIL
+export PRODUCTION_KEY=random_key # Can be whatever
+export USE_STATIC_FILES=false # Set this to true during production
+export WEBSITE=http://localhost:8000/API/activate_user/ # Used a link for activating a user
+export EMAIL_HOST=mail.gandi.net
+export EMAIL_PORT=587
+export EMAIL_USERNAME=noreply@uniquiz.se
+export EMAIL_PASSWORD=Emilojonisarbast9593@
--- scripts/lock.sh
+++ scripts/lock.sh
@@ -8,8 +8,8 @@ case "$1" in
   -l | --lock)
 
   ffmpeg -y -loglevel quiet -s 2880x1800 -f x11grab -i $DISPLAY -vframes 1 \
-  -vf "boxblur=luma_radius=6:luma_power=3, \
-      drawbox=x=25:y=1692:w=300:h=80:color=black@0.5:t=fill" $TMPBG
+  -vf "boxblur=luma_radius=3:luma_power=3, \
+  drawbox=x=iw-(0.99*iw):y=ih-(0.06*ih):w=300:h=85:color=black@0.5:t=fill" $TMPBG
 
   i3lock \
   -i "$TMPBG" \
@@ -23,9 +23,24 @@ case "$1" in
   --radius=20 --ring-width=3 --veriftext="" --wrongtext="" \
   --layoutcolor="ffffffff" --timecolor="ffffffff" --datecolor="ffffffff" \
   --timesize=22 --datesize=14
-   rm $TMPBG
-   ;;
+  rm $TMPBG
+  ;;
 
+  -b | --blur)
+
+  i3lock \
+  --blur=3 \
+  --timepos="x+75:h-44" \
+  --datepos="tx+6:ty+20" \
+  --clock --timestr="%l:%M %P" --datestr "Type password..." \
+  --insidecolor=00000000 --ringcolor=ffffffff --line-uses-inside \
+  --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
+  --insidevercolor=1edb34ff --insidewrongcolor=d23c3dff \
+  --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+185:h-44" \
+  --radius=20 --ring-width=3 --veriftext="" --wrongtext="" \
+  --layoutcolor="ffffffff" --timecolor="ffffffff" --datecolor="ffffffff" \
+  --timesize=22 --datesize=14
+  ;;
 esac
 
 
